6f544b7570a053916aead0ecfc9f0beda26be214
fsponton <francisco.sponton@gmail.com>
2023-05-18 15:40:06 -0300

    configuracion de gateway y apis en docker, los 4 contenedores conectados en la misma red

--- gateway/index.js
+++ gateway/index.js
@@ -4,18 +4,18 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 const app = express();
 
 app.use('/characters', createProxyMiddleware({
-    target: "http://localhost:8001",
+    target: "http://api-characters:8001",
     changeOrigin: true
 }));
 
 
 app.use("/films", createProxyMiddleware({
-    target: "http://localhost:8002",
+    target: "http://api-films:8002",
     changeOrigin: true
 }));
 
 app.use("/planets", createProxyMiddleware({
-    target: "http://localhost:8003",
+    target: "http://api-planets:8003",
     changeOrigin: true
 }));
 
